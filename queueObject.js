cf1d2f9b758f5d064913557d5d422e0236b5725d
TranVanSu2001 <80629567+TranVanSu2001@users.noreply.github.com>
2021-07-12 00:57:44 +0700

    Update queueObject.js

--- queueObject.js
+++ queueObject.js
@@ -14,7 +14,7 @@ class queueObject {
   }
 
   dequeue() {
-    if((this.tailIndex - this.headIndex) === 0) {
+    if(this.tailIndex = this.headIndex) {
       console.log('This is empty') // nếu không có phần tử nào thì báo trống
     } else {
       const item = this.items[this.headIndex];
