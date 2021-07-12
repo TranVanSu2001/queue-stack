b387bb7e89a9014b60b158c9574fa87134505de7
TranVanSu2001 <80629567+TranVanSu2001@users.noreply.github.com>
2021-07-12 15:00:42 +0700

    Update stackObject.js

--- stackObject.js
+++ stackObject.js
@@ -33,7 +33,7 @@ class stackObject {
   }
   
   isEmpty() {
-    if( this.tailIndex = this.headIndex) {
+    if( this.tailIndex === this.headIndex) {
       return true;
     } else {
       return false;
